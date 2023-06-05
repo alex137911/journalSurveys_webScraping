ff810247b8d9adf4db66bbc62cb8af093e48ca9c
Alex Chan <alex.chan1@uwaterloo.ca>
2023-06-05 14:21:14 +0100

    update (06/05/2023)

---
+++ webScraping.R
@@ -0,0 +1,122 @@
+# Script to scrape names of editors-in-chief
+
+# Remove objects in workspace
+rm(list = ls())
+
+# Required packages
+suppressMessages(library(rvest))
+suppressMessages(library(dplyr))
+suppressMessages(library(stringi))
+suppressMessages(library(data.table))
+suppressMessages(library(stringr))
+
+# -------------------------------------------------------------------
+# Specify the URL of the website to scrape
+url <- "https://www.sciencedirect.com/browse/journals-and-books?page=1&contentType=JL&subject=medicine-and-dentistry"
+
+# Retrieve the HTML content of website
+html <- read_html(url)
+
+# Pull journal names
+journalNode <- html_nodes(html, ".js-publication")
+journalName <- html_text(journalNode)
+
+# Store as data frame
+journalName_df <- as.data.frame(journalName)
+
+# Science direct journal URL template: www.sciencedirect.com/journal/journal-name
+# Remove characters that are not part of journal titles
+journalName_df$journalName <- gsub("\\•..*", "", journalName_df$journalName)
+
+# Remove "Journal" from titles (i.e., last 7 characters of each string)
+removeLast_n <- 7
+journalName_df$journalName <- str_trim(journalName_df$journalName, side = "right")
+journalName_df$journalName <- substr(journalName_df$journalName, 1, 
+                                     nchar(journalName_df$journalName) - removeLast_n)
+  
+# Remove parentheses from journal titles
+journalName_df$journalName <- gsub("\\(|\\)", "", journalName_df$journalName)
+
+# Replace "&" with "and" in journal titles
+journalName_df$journalName <- gsub("\\&", "and", journalName_df$journalName)
+
+# Remove " - " in journal titles  
+journalName_df$journalName <- gsub("\\ - ", " ", journalName_df$journalName)
+
+# Remove apostrophe (') in journal titles 
+journalName_df$journalName <- gsub("\\'", "", journalName_df$journalName)
+
+# Remove ":" in journal titles
+journalName_df$journalName <- gsub("\\:", "", journalName_df$journalName)
+
+# Replace spaces with "-"
+journalName_df$journalName <- gsub(" ", "-", journalName_df$journalName)
+
+# Store as data table (necessary for following operation)
+journalName_df <- as.data.table(journalName_df)
+
+# Remove accents from journal titles (e.g., English versions of foreign journals)
+# https://stackoverflow.com/a/56595128/15269111
+journalName_df <- journalName_df[, journalName := stri_trans_general(str = journalName, id = "Latin-ASCII")]
+
+# Remove white spaces
+journalName_df$journalName <- str_trim(journalName_df$journalName, side = "right")
+
+# Create URLs
+journalName_df$journalURL <- NA
+
+for(i in 1:nrow(journalName_df)){
+  journalName_df$journalURL[i] <- paste0("www.sciencedirect.com/journal/", "", journalName_df$journalName[i])
+}
+
+# -------------------------------------------------------------------
+# Editor URLs
+journalName_df$chiefEditor <- NA
+
+for(i in 1:nrow(journalName_df)){
+  editorURL <- journalName_df$journalURL[i]
+  
+  # Check if the URL is missing the protocol and prepend "https://"
+  if(!grepl("^https?://", editorURL, ignore.case = TRUE)) {
+    editorURL <- paste0("https://", editorURL)
+  }
+  
+  tryCatch({
+    # Retrieve the HTML content of website
+    editorHTML <- read_html(editorURL)
+    
+    # Pull editor
+    editorNode <- html_nodes(editorHTML, ".js-editor-name")
+    editorName <- html_text(editorNode)
+    
+    # Check if "editorName" is empty (e.g., if journal is no longer in print)
+    if(editorName != "") {
+      journalName_df$chiefEditor[i] <- editorName
+    } 
+    else {
+      journalName_df$chiefEditor[i] <- "NA"
+    }
+  }, error = function(e){
+    
+    # Handle any errors that occur during scraping
+    journalName_df$chiefEditor[i] <- "Error: Unable to scrape"
+    print(paste("Error scraping", editorURL, ":", conditionMessage(e)))
+    
+  }) # end of error function
+}
+
+# Output directory
+outpath <- "C:/Users/acale/OneDrive/Documents/Waterloo BME/Co-op/OHRI Research Internship/journalSurveys_webScraping/Data"
+outDir <- sprintf("%s/Data/webScraping.R", dirname(outpath))
+if(!file.exists(outDir)) dir.create(outDir)
+
+write.table(journalName_df, file = sprintf("%s/scienceDirect_chiefEditor.tsv", outDir),
+            sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+
+# # Pull editor
+# editorNode <- html_nodes(editorHTML, ".js-editor-name")
+# editorName <- html_text(editorNode)
+# 
+# print(editorName)
+# print(journalName_df$journalURL)
