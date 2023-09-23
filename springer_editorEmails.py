c19d7b5364d1035f08f57108615dfbf98bba789a
Alex Chan <alex.chan1@uwaterloo.ca>
2023-09-23 15:00:03 -0400

    Editor-in-Chief names Springer

--- springer_editorEmails.py
+++ springer_editorEmails.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from fuzzywuzzy import fuzz
 
+import csv
 import pandas as pd
 import time
 import os
@@ -18,51 +19,72 @@ import re
 os.chdir("C:/Users/acale/OneDrive/Documents/Waterloo BME/Co-op/OHRI Research Internship/journalSurveys_webScraping/Data/webScraping.R")
 
 # -------------------------------------------------------------------
-# Function to calculate the match rate of characters in two strings
-def calculate_match(name, email):
-    return fuzz.token_sort_ratio(name.lower(), email.lower())
-
-# Initialize variables to keep track of the best match
-best_match_rate = 0
-best_match_email = ""
-
-# Function to relocate the search bar element
-# Necessary to prevent stale element reference exception
-def find_search_bar():
-    while True:
-        try:
-            return driver.find_element('xpath','//*[(@id = "id_term")]')
-        except NoSuchElementException:
-            time.sleep(1)  # Wait for 1 second before retrying
-
-# Function to clear the search bar
-# search_bar.clear() is not working
-def clear_search_bar():
-    search_bar = driver.find_element('xpath','//*[(@id = "id_term")]')
-    search_bar.send_keys(Keys.CONTROL + "a")  # Select all text in the search bar
-    driver.implicitly_wait(10)
-    search_bar.send_keys(Keys.DELETE)  # Delete the selected text
-    driver.implicitly_wait(10)
-
-# -------------------------------------------------------------------
 # Specify the path to the Chromedriver executable
 chromedriver_path = 'C:\Program Files\chromedriver-win64\chromedriver.exe'
 
 # Create new instance of the Chrome driver
-driver = webdriver.Chrome(executable_path = chromedriver_path)
+driver = webdriver.Chrome(executable_path=chromedriver_path)
 
 # Maximize the browser window to make it fullscreen
-# Can sometimes help reduce errors from popups blocking elements
 driver.maximize_window()
-driver.get("https://link.springer.com/search/page/1?facet-discipline=%22Biomedicine%22&facet-content-type=%22Journal%22")
-article = driver.find_element('xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
-article.click()
 
-# Locate the element containing the Editor-in-Chief's name
-editor_element = driver.find_element('xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "u-list-inline", " " ))]')
+# Initialize CSV file
+with open('springer_biomedicine.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["journalName", "journalURL", "chiefEditor"])
 
-# Extract the name
-editor_name = editor_element.text
-print(f"Editor-in-Chief: {editor_name}")
+    # Loop through each of the 12 pages
+    for page_num in range(1, 13):
+        driver.get(f"https://link.springer.com/search/page/{page_num}?facet-discipline=%22Biomedicine%22&facet-content-type=%22Journal%22")
+        
+        # Get all journal links on the current page
+        articles = driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
+        
+        for article in articles:
+            # Extract journal name
+            journal_name = article.text
+            
+            # Click on the journal link
+            article.click()
+            
+            # Extract journal URL
+            journal_url = driver.current_url
+            
+            # Locate the element containing the Editor-in-Chief's name
+            try:
+                editor_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "u-list-inline", " " ))]')))
+                chief_editor = editor_element.text
+            except:
+                chief_editor = "Not Found"
+            
+            # Write to CSV
+            writer.writerow([journal_name, journal_url, chief_editor])
+            
+            # Go back to the list of journals
+            driver.back()
+
+# Close the browser
+driver.quit()
+
+# -------------------------------------------------------------------
+# # Specify the path to the Chromedriver executable
+# chromedriver_path = 'C:\Program Files\chromedriver-win64\chromedriver.exe'
+
+# # Create new instance of the Chrome driver
+# driver = webdriver.Chrome(executable_path = chromedriver_path)
+
+# # Maximize the browser window to make it fullscreen
+# # Can sometimes help reduce errors from popups blocking elements
+# driver.maximize_window()
+# driver.get("https://link.springer.com/search/page/1?facet-discipline=%22Biomedicine%22&facet-content-type=%22Journal%22")
+# article = driver.find_element('xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
+# article.click()
+
+# # Locate the element containing the Editor-in-Chief's name
+# editor_element = driver.find_element('xpath', '//*[contains(concat( " ", @class, " " ), concat( " ", "u-list-inline", " " ))]')
+
+# # Extract the name
+# editor_name = editor_element.text
+# print(f"Editor-in-Chief: {editor_name}")
 
 
