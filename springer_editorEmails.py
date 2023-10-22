813e59a839f1cd2a79321e203730a13c9346954e
Alex Chan <alex.chan1@uwaterloo.ca>
2023-10-22 01:54:37 -0400

    Springer journals scraping

--- springer_editorEmails.py
+++ springer_editorEmails.py
@@ -7,9 +7,7 @@ from selenium.webdriver.common.by import By
 from fuzzywuzzy import fuzz
 
 import os
-# import threading
 import csv
-import concurrent.futures
 
 # -------------------------------------------------------------------
 # Set working directory
@@ -17,9 +15,12 @@ import concurrent.futures
 os.chdir("C:/Users/acale/OneDrive/Documents/Waterloo BME/Co-op/OHRI Research Internship/journalSurveys_webScraping/Data/webScraping.R")
 
 # -------------------------------------------------------------------
+# Function to retrieve all article elements on the current page
+# Identies articles based on HTML "title" class
 def get_articles(driver):
     return driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
 
+# Function to accept cookies if they appear
 def accept_cookies(driver):
     try:
         banner_button = driver.find_element_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "cc-banner__button-accept", " " ))]')
@@ -27,6 +28,7 @@ def accept_cookies(driver):
     except NoSuchElementException: 
         pass
 
+# Function to navigate to the article page and extract the journal name, URL, and chief editor
 def navigate_and_extract_data(article, driver):
     try:
         journal_name = article.text
@@ -38,7 +40,10 @@ def navigate_and_extract_data(article, driver):
     except:
         return None, None, None
 
+# Function to scrape the data from each article
 def scrape_journal_data(driver, writer):
+
+    # Calls the get_articles function to retrieve all articles on the page
     articles = get_articles(driver)
     i = 0
     while i < len(articles):
@@ -46,16 +51,21 @@ def scrape_journal_data(driver, writer):
             accept_cookies(driver)
             article = get_articles(driver)[i]
             
+            # Extracts journal name, URL, and chief editor
             journal_name, journal_url, chief_editor = navigate_and_extract_data(article, driver)
-            
+
             if journal_name and journal_url and chief_editor:
                 writer.writerow([journal_name, journal_url, chief_editor])
             
             driver.back()
             i += 1
+
+        # If the article element is no longer attached to the DOM, refetch the articles
         except StaleElementReferenceException:
             print(f"Stale element on article {i}. Refetching articles.")
             articles = get_articles(driver)
+            
+        # Skip the article if an error is encountered
         except Exception as e:
             print(f"Error encountered: {e}. Skipping article {i}.")
             i += 1
@@ -76,99 +86,19 @@ driver.set_page_load_timeout(120)
 # Maximize the browser window to make it fullscreen
 driver.maximize_window()
 
-# Extract the names of the Editors-in-Chief and store in TSV
-with open('springer_biomedicine.tsv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file, delimiter="\t")
-    writer.writerow(["journalName", "journalURL", "chiefEditor"])
+try:
+    # Creats TSV to store data
+    with open('springer_biomedicine.tsv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter="\t")
+        writer.writerow(["journalName", "journalURL", "chiefEditor"])
 
-    # Loop through each of the 12 pages
-    for page_num in range(1, 13):
-        driver.get(f"https://link.springer.com/search/page/{page_num}?facet-discipline=%22Biomedicine%22&facet-content-type=%22Journal%22")
-        scrape_journal_data(driver, writer)
+        # Loop through each of the 12 pages
+        for page_num in range(1, 13):
+            driver.get(f"https://link.springer.com/search/page/{page_num}?facet-discipline=%22Biomedicine%22&facet-content-type=%22Journal%22")
+            scrape_journal_data(driver, writer)
 
-# Close the browser
-driver.quit()
+finally:
+    # Close the browser
+    driver.quit()
 
 # -------------------------------------------------------------------
-
-# # Specify the path to the Chromedriver executable
-# chromedriver_path = 'C:\Program Files\chromedriver-win64\chromedriver.exe'
-
-# # Create new instance of the Chrome driver
-# options = webdriver.ChromeOptions()
-# options.add_argument("--disable-device-discovery-notifications")
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
-# driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
-# # driver = webdriver.Chrome(executable_path=chromedriver_path)
-
-# # Set timeout to 2 minutes
-# # driver.set_page_load_timeout(120)  
-
-# # Maximize the browser window to make it fullscreen
-# driver.maximize_window()
-
-# # Initialize TSV file
-# with open('springer_biomedicine.tsv', 'w', newline = '', encoding = 'utf-8') as file:
-#     writer = csv.writer(file, delimiter = "\t")
-#     writer.writerow(["journalName", "journalURL", "chiefEditor"])
-
-#     # Loop through each of the 12 pages
-#     for page_num in range(1, 13):
-#         driver.get(f"https://link.springer.com/search/page/{page_num}?facet-discipline=%22Biomedicine%22&facet-content-type=%22Journal%22")
-        
-#         # Get all journal links on the current page
-#         articles = driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
-        
-#         # Use an index to loop through articles
-#         i = 0
-#         while i < len(articles):
-#             # Record the current time before navigating to a journal page
-#             start_time = time.time()
-
-#             # "Accept cookies" banner
-#             try:
-#                 try:
-#                     banner_button = driver.find_element_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "cc-banner__button-accept", " " ))]')
-#                     banner_button.click()
-#                     # Re-fetch the list of articles after closing the banner
-#                     articles = driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
-                
-#                 except NoSuchElementException: 
-#                     pass  # If the banner button is not found, just continue
-                
-#                 # Click on the current article using the index
-#                 article = articles[i]
-#                 journal_name = article.text
-                
-#                 # Click on the current article using the index
-#                 article.click()
-                
-#                 # Extract journal URL
-#                 journal_url = driver.current_url
-
-#                 # Locate the element containing the Editor-in-Chief's name
-#                 try:
-#                     editor_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(concat( " ", @class, " " ), concat( " ", "u-list-inline", " " ))]')))
-#                     chief_editor = editor_element.text
-                
-#                 except:
-#                     chief_editor = "Not Found"
-                
-#                 # Write to TSV
-#                 writer.writerow([journal_name, journal_url, chief_editor])
-                
-#                 # Go back to the list of journals
-#                 driver.back()
-                
-#                 # Re-fetch the list of articles after navigating back
-#                 articles = driver.find_elements_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "title", " " ))]')
-                
-#                 # Increment the index to process the next article
-#                 i += 1
-
-#             except Exception as e:  # Catch all exceptions
-#                 print(f"Error encountered: {e}. Skipping current journal.")
-#                 i += 1  # Increment the index to process the next article
-                
-# # Close the browser
-# driver.quit()
